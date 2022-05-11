af4982c56afe7801a1be6212f61fe3915e671601
moon <no1msh1217@gmail.com>
2022-05-11 17:55:05 +0900

    Day3 chapter1_input_and_output_calculation
    No_1000 : A + B
    No_10717 : 고양이
    No_25083 : 새싹

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter1_input_and_output_calculation/No_10171.java
@@ -0,0 +1,12 @@
+package chapter1_input_and_output_calculation;
+
+public class No_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
