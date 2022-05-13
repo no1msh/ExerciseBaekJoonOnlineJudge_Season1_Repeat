5b57a91153a4a1a3af4e8f123dd96fe390ee2420
TFQ_Moon <no1msh1217@gmail.com>
2022-05-13 23:35:39 +0900

    Day4 chapter1_input_and_output_calculation
    No_10998 : A x B

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter1_input_and_output_calculation/No_10998.java
@@ -0,0 +1,16 @@
+package chapter1_input_and_output_calculation;
+
+import java.io.*;
+
+public class No_10998 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputStr = br.readLine().split(" ");
+		int a = Integer.parseInt(inputStr[0]);
+		int b = Integer.parseInt(inputStr[1]);
+		
+		System.out.println(a*b);
+	}
+
+}
