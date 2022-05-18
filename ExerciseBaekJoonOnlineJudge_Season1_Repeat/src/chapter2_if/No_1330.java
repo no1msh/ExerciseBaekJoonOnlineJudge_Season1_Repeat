297ff035500d85decfabd4944625d82a403ef6eb
moon <no1msh1217@gmail.com>
2022-05-18 23:40:12 +0900

    Day10 chapter2_if
    No_1330 : 두 수 비교하기

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter2_if/No_1330.java
@@ -0,0 +1,24 @@
+package chapter2_if;
+
+import java.io.*;
+
+public class No_1330 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] inputStr = br.readLine().split(" ");
+		
+		int a = Integer.parseInt(inputStr[0]);
+		int b = Integer.parseInt(inputStr[1]);
+		
+		if (a > b) {
+			System.out.println(">");
+		} else if (a < b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+
+}
