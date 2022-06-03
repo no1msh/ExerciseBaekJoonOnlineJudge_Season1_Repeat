10c9e80296d195bb2b295fecc1ff3b4893d5b48a
moon <no1msh1217@gmail.com>
2022-06-03 14:14:02 +0900

    Day26 chapter3_loop & chapter4_array
    - chapter3_loop
    No_1110 : 더하기 싸이클
    No_10871 : x 보다 작은 수
    No_10951 : A + B - 4
    No_10952 : A + B - 5
    
    - chapter4_array
    No_10818 : 최소 최대

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter3_loop/No_10951.java
@@ -0,0 +1,23 @@
+package chapter3_loop;
+
+import java.io.*;
+
+public class No_10951 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputStr = "";
+
+		while ((inputStr = br.readLine()) != null) {
+			
+			String[] inputNum = inputStr.split(" ");
+
+			int a = Integer.parseInt(inputNum[0]);
+			int b = Integer.parseInt(inputNum[1]);
+
+			System.out.println(a + b);
+		}
+	}
+
+}
