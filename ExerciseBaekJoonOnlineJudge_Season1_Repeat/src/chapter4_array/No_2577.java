5bf174954c11332fb21db2e6c305d7702e39d640
moon <no1msh1217@gmail.com>
2022-06-05 00:14:31 +0900

    Day27 chapter4_array
    No_2562 : 최댓값
    No_2577 : 숫자의 개

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter4_array/No_2577.java
@@ -0,0 +1,36 @@
+package chapter4_array;
+
+import java.io.*;
+
+public class No_2577 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int result = a * b * c;
+		
+		String resultStr = String.valueOf(result);
+		
+		char[] numCharArr = {'0','1','2','3','4','5','6','7','8','9'};
+		
+		for (int i = 0 ; i < numCharArr.length; i++) {
+			int count = 0;
+			
+			for (int j = 0 ; j < resultStr.length(); j++) {
+				if (numCharArr[i] == resultStr.charAt(j)) {
+					count++;
+				}
+			}
+			
+			System.out.println(count);
+		}
+		
+		
+		
+	}
+
+}
