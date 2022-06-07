76cd98ec77dda09e86cc6cf9bcb7bd91b9174f7b
moon <no1msh1217@gmail.com>
2022-06-07 22:44:07 +0900

    Day29 chapter4_array
    No_8958 : OX

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter4_array/No_8958.java
@@ -0,0 +1,35 @@
+package chapter4_array;
+
+import java.io.*;
+
+public class No_8958 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+
+			String inputResult = br.readLine();
+			int combo = 1;
+			int score = 0;
+
+			for (int j = 0; j < inputResult.length(); j++) {
+
+				if (inputResult.charAt(j) == 'O') {
+					score += combo;
+					combo++;
+					
+				} else {
+					combo = 1;
+				}
+
+			}
+			System.out.println(score);
+		}
+
+	}
+
+}
