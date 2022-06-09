6239b00a259542c7cfecf5c8b4c75d64fa7f8b0f
moon <no1msh1217@gmail.com>
2022-06-09 10:13:44 +0900

    Day31 chapter5 method & IntelliJ Init
    No_15596 : 정수 N개의 합

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter5_method/No_15596.java
@@ -0,0 +1,15 @@
+package chapter5_method;
+
+public class No_15596 {
+
+    long sum(int[] a){
+        long result = 0;
+
+        for (int value : a){
+            result += value;
+        }
+
+        return result;
+    }
+
+}
