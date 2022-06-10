4973cc682d2d8a62f2986428fa96ef0fb9c67624
moon <no1msh1217@gmail.com>
2022-06-10 10:49:36 +0900

    Day32 chapter5 method
    No_4673 : 셀프 넘버

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter5_method/No_4673.java
@@ -0,0 +1,40 @@
+package chapter5_method;
+
+public class No_4673 {
+
+    static int drFunc(int num) {
+
+        int inputNum = num;
+        int result = num;
+
+        while (true) {
+            result += inputNum % 10;
+            inputNum /= 10;
+
+            if (inputNum < 10) {
+                result += inputNum;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        boolean[] isSelfNumArr = new boolean[10001];
+
+        for (int i = 1; i < isSelfNumArr.length; i++) {
+            if (drFunc(i) <= 10000)
+                isSelfNumArr[drFunc(i)] = true;
+
+        }
+
+        for (int j = 1; j < isSelfNumArr.length; j++) {
+            if (!isSelfNumArr[j]) {
+                System.out.println(j);
+            }
+        }
+
+    }
+}
