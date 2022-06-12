7b6b2e334042a1d0b964d3c8f9a76b536e804433
moon <no1msh1217@gmail.com>
2022-06-12 12:31:18 +0900

    Day34 chapter6 string
    No_11654 : 아스키 코드
    No_11720 : 숫자의 합
    No_10809 : 알파벳 찾기
    No_10809_ver2 : 알파벳 찾기 다른 풀이

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter6_string/No_10809_ver2.java
@@ -0,0 +1,29 @@
+package chapter6_string;
+
+import java.io.*;
+
+public class No_10809_ver2 {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputStr = br.readLine();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0 ; i < alphabet.length(); i++){
+            boolean include = false;
+            for (int j = 0 ; j < inputStr.length() ; j++){
+                if (alphabet.charAt(i) == inputStr.charAt(j)){
+                    include = true;
+                    System.out.print(j+" ");
+                    break;
+                }
+            }
+
+            if (!include)
+                System.out.print(-1 + " ");
+        }
+
+    }
+}
---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter6_string/No_11654.java
@@ -0,0 +1,13 @@
+package chapter6_string;
+
+import java.io.*;
+
+public class No_11654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputStr = br.readLine();
+
+        System.out.print((int)inputStr.charAt(0));
+    }
+}
