50bc788c68726c3e1cbf2c0afbb28ba3ae62decb
moon <no1msh1217@gmail.com>
2022-06-16 23:46:29 +0900

    Day38 chapter6 string
    No_2941 : 크로아티아 알파벳

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter6_string/No_2941.java
@@ -0,0 +1,56 @@
+package chapter6_string;
+
+import java.io.*;
+
+public class No_2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputStr = br.readLine();
+        int count = inputStr.length();
+
+        for (int i = 0; i < inputStr.length() - 1; i++) {
+
+            if (inputStr.charAt(i) == 'c') {
+                if (inputStr.charAt(i + 1) == '=') {
+                    count--;
+                } else if (inputStr.charAt(i + 1) == '-') {
+                    count--;
+                }
+
+            } else if (inputStr.charAt(i) == 'd') {
+                if (inputStr.charAt(i + 1) == 'z') {
+                    if ((i + 2) < inputStr.length()) {
+                        if (inputStr.charAt(i + 2) == '=') {
+                            count -= 2;
+                        }
+                    }
+                } else if (inputStr.charAt(i + 1) == '-') {
+                    count--;
+                }
+            } else if (inputStr.charAt(i) == 'l') {
+                if (inputStr.charAt(i + 1) == 'j') {
+                    count--;
+                }
+            } else if (inputStr.charAt(i) == 'n') {
+                if (inputStr.charAt(i + 1) == 'j') {
+                    count--;
+                }
+            } else if (inputStr.charAt(i) == 's') {
+                if (inputStr.charAt(i + 1) == '=') {
+                    count--;
+                }
+            } else if (inputStr.charAt(i) == 'z') {
+                if (inputStr.charAt(i + 1) == '=') {
+                    if (0 <= i - 1) {
+                        if (inputStr.charAt(i - 1) == 'd') {
+                            continue;
+                        }
+                    }
+                    count--;
+                }
+            }
+
+        }
+        System.out.print(count);
+    }
+}
