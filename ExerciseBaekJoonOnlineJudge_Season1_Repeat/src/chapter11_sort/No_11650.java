f08e66195949c20144b5bd50ff19aa2061763b94
moon <no1msh1217@gmail.com>
2022-07-12 20:17:01 +0900

    Day64 chapter11_sort
    No_11650 : 좌표 정렬하기

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter11_sort/No_11650.java
@@ -0,0 +1,33 @@
+package chapter11_sort;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class No_11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int testCase = Integer.parseInt(br.readLine());
+        int[][] array = new int[testCase][2];
+
+        for (int i = 0; i < testCase; i++) {
+            String[] inputString = br.readLine().split(" ");
+            array[i][0] = Integer.parseInt(inputString[0]);
+            array[i][1] = Integer.parseInt(inputString[1]);
+        }
+
+        Arrays.sort(array, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i][0]).append(" ").append(array[i][1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
