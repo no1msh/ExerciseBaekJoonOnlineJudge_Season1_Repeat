673450aa985e029fd23375ea0d31b17c200625ea
moon <no1msh1217@gmail.com>
2022-07-23 19:26:42 +0900

    Day75 chapter14_number_theory
    No_1037 : 약수

---
+++ ExerciseBaekJoonOnlineJudge_Season1_Repeat/src/chapter14_number_theory/No_1037.java
@@ -0,0 +1,27 @@
+package chapter14_number_theory;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class No_1037 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        while (count-- > 0) {
+            int N = Integer.parseInt(st.nextToken());
+            max = Math.max(N, max);
+            min = Math.min(N, min);
+        }
+
+        System.out.println(max*min);
+    }
+
+}
